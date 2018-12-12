4044858f8760fba1ed40e582434ae01fe06bb83f
Shubhendu Pramanik <subho.pramanik1@gmail.com>
2018-12-12 07:22:21 +0530

    Added java docs and implemented JUnit

--- src/main/java/com/techprimers/controller/UsersController.java
+++ src/main/java/com/techprimers/controller/UsersController.java
@@ -11,19 +11,37 @@ import java.util.List;
 @RequestMapping("/users")
 public class UsersController {
 
+	/** The JPA repository */
     @Autowired
     private UserJpaRespository userJpaRespository;
 
+	/**
+	 * Used to fetch all the users from DB
+	 * 
+	 * @return list of {@link Users}
+	 */
     @GetMapping(value = "/all")
     public List<Users> findAll() {
         return userJpaRespository.findAll();
     }
 
+    /**
+	 * Used to find and return a user by name
+	 * 
+	 * @param name refers to the name of the user
+	 * @return {@link Users} object
+	 */
     @GetMapping(value = "/{name}")
     public Users findByName(@PathVariable final String name){
         return userJpaRespository.findByName(name);
     }
 
+    /**
+	 * Used to create a User in the DB
+	 * 
+	 * @param users refers to the User needs to be saved
+	 * @return the {@link Users} created
+	 */
     @PostMapping(value = "/load")
     public Users load(@RequestBody final Users users) {
         userJpaRespository.save(users);
